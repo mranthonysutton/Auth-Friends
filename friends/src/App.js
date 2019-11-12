819a5aef8f6af6c0502236037cf483af99970e91
Anthony Sutton <anthony.sutton95@gmail.com>
2019-11-12 13:29:08 -0800

    Created Routes and added Friend List page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
 import './App.css';
 import LoginForm from "./components/LoginForm";
+import PrivateRoute from "./components/PrivateRoute";
+import FriendList from "./components/FriendList";
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm/>
-    </div>
-)
-  ;
+    <Router>
+      <div className="App">
+        <Link to="/login">Login</Link>
+        <Link to="/friends">Friend List</Link>
+
+      <Switch>
+        <Route path="/login" component={LoginForm} />
+        <Route path="/friends" component={FriendList} />
+      </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ friends/src/components/FriendList.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const FriendList = () => {
+  return <h1>From The Friend List</h1>
+};
+
+export default FriendList;
